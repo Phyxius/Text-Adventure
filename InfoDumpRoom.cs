5d4da2a274078a34951602da30e5740e80ca4864
Phyxius <env.laser@gmail.com>
2012-03-27 17:44:20 -0700

    Finished inventory!

--- InfoDumpRoom.cs
+++ InfoDumpRoom.cs
@@ -5,37 +5,45 @@ using System.Text;
 
 namespace CandideTextAdventure
 {
-    class InfoDumpRoom : Room
+    internal class InfoDumpRoom : Room
     {
-        private Dictionary<InfoDumpPainting, bool> paintings = new Dictionary<InfoDumpPainting, bool>(); 
+        private Dictionary<InfoDumpPainting, bool> paintings = new Dictionary<InfoDumpPainting, bool>();
         private Room next;
-        public InfoDumpRoom(IEnumerable<InfoDumpPainting> list, Room nextroom)
+        private string ending;
+
+        public InfoDumpRoom(IEnumerable<InfoDumpPainting> list, Room nextroom, string endingmessage = "You feel yourself being sucked through a magical portal...")
         {
             foreach (InfoDumpPainting p in list)
                 paintings.Add(p, false);
             Objects.AddRange(list);
             next = nextroom;
-            Objects.Add(new TestPickup());
+            ending = endingmessage;
+            //Objects.Add(new TestPickup());
+            //Objects.Add(new TestUsable());
         }
+
         public override bool OnInteract(string command, Item target)
         {
-            if (command == "examine" && target.GetType() == typeof(InfoDumpPainting) && paintings.ContainsKey((InfoDumpPainting)target))
+            if (command == "examine" && target.GetType() == typeof (InfoDumpPainting) &&
+                paintings.ContainsKey((InfoDumpPainting) target))
             {
                 paintings[(InfoDumpPainting) target] = true;
                 if (!paintings.ContainsValue(false))
                 {
                     target.OnInteract("examine");
-                    Console.WriteLine("You feel yourself being sucked through a magical portal...");
+                    Console.WriteLine(ending);
                     ChangeRoom(next);
                     return false;
-                }   
+                }
             }
             return true;
         }
+
         public override void OnEnter()
         {
             Console.WriteLine("Examine all paintings to continue");
         }
+
         public override bool OnExamine()
         {
             Console.WriteLine("You are in a room with paintings.");
@@ -43,9 +51,10 @@ namespace CandideTextAdventure
         }
     }
 
-    class InfoDumpPainting : Item
+    internal class InfoDumpPainting : Item
     {
         private readonly string description;
+
         public InfoDumpPainting(string name, string description)
         {
             this.description = description;
@@ -53,6 +62,7 @@ namespace CandideTextAdventure
             ValidNames.Add("painting of " + name);
             ValidNames.Add("a painting of " + name);
         }
+
         public override bool OnInteract(string command)
         {
             switch (command)
@@ -64,18 +74,20 @@ namespace CandideTextAdventure
                     return false;
             }
         }
+
         public override string GetName()
         {
             return "a painting of " + ValidNames[0];
         }
     }
 
-    class TestPickup : Item
+    internal class TestPickup : Item
     {
         public TestPickup(string name = "item")
         {
             ValidNames.Add(name);
         }
+
         public override bool AttemptedGrab()
         {
             Console.WriteLine("You pick it up.");
@@ -83,5 +95,34 @@ namespace CandideTextAdventure
             Room.CurrentRoom.Objects.Remove(this);
             return true;
         }
+        public override bool AttemptedSingleUse()
+        {
+            Console.WriteLine("You use it on itself");
+            return true;
+        }
+
+        public override bool OnInteract(string command)
+        {
+            if (command == "examine")
+            {
+                Console.WriteLine("It is an item.");
+                return true;
+            }
+            return false;
+        }
+        public override bool AttemptedDoubleItemUse(Item target)
+        {
+            Console.WriteLine("You use it.");
+            return true;
+        }
+    }
+
+    class TestUsable : Item
+    {
+        public TestUsable(string name = "usable")
+        {
+            ValidNames.Add(name);
+        }
+        
     }
 }
